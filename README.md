6af913b1b6c4b6cf3cbb127e63dc5261886a0eed
samar2609 <87849878+samar2609@users.noreply.github.com>
2021-12-05 17:33:43 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# dragon-v-vampire-game
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,88 @@
+var dragon,dragonImg
+var vampire,vampireImg
+var fireball, fireBallImg 
+var background1,backgroundImg 
+
+
+function preload(){
+dragonImg = loadImage("Dragon.png")
+vampireImg = loadImage("Vampire.jpeg")
+fireBallImg = loadImage("fire ball.jpeg ")
+backgroundImg = loadImage("bg.png")
+}
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+ 
+  background1 = createSprite(600,300,400,20)
+  background1.addImage(backgroundImg)
+  backgroundImg.scale = 1.15
+
+  dragon = createSprite(50,300)
+  dragon.addImage(dragonImg)
+  dragon.scale = 0.09
+
+  vampireGroup = new Group()
+  fireballGroup = new Group()
+}
+
+function draw() {
+  background(255,255,255); 
+
+  spawnVampires()
+  
+  if(keyDown("Up_Arrow")){
+    dragon.y = dragon.y -3
+    }
+    
+    if(keyDown("Down_Arrow")){
+        dragon.y = dragon.y +3
+    }
+
+    if(keyDown("Right_Arrow")){
+      dragon.x = dragon.x +3
+  }
+
+    if(vampireGroup.isTouching(dragon)){
+      dragon.destroy()
+  }
+
+    if(keyWentDown("space")){
+      fireball = createSprite(100,100)
+      fireball.x = dragon.x
+      fireball.y = dragon.y
+      fireball.addImage(fireBallImg)
+      fireball.scale = 0.1
+      fireball.velocityX = 5
+      
+fireballGroup.add(fireball)
+      }
+
+      function spawnVampires(){
+        if(frameCount % 80 === 0){
+          vampire = createSprite(500,100)
+          vampire.x = random(500,800)
+          vampire.y = random(10,300)
+          vampire.addImage(vampireImg)
+          vampire.scale = 0.09
+          vampire.velocityX = -1
+        
+          vampireGroup.add(vampire)
+        }
+        }
+
+
+
+for(var i = 0; i<vampireGroup.length; i++){
+if(vampireGroup[i].isTouching(fireballGroup)){
+vampireGroup[i].destroy()
+fireballGroup.destroyEach()
+}
+}
+
+
+  drawSprites();
+}
